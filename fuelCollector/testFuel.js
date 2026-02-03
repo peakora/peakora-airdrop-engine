fc50e615dde99fb4f66bd3b0aad697b5fedb4b2c
peakora <peakora.network@gmail.com>
2026-02-03 14:18:44 +0300

    Update testFuel.js

--- fuelCollector/testFuel.js
+++ fuelCollector/testFuel.js
@@ -1,7 +1,9 @@
 const { collectRewards } = require("./fuelCollector");
 
 async function main() {
+  console.log("=== Fuel Collector Test ===");
   await collectRewards("WALLET_KEY_1");
+  console.log("=== Test Finished ===");
 }
 
 main();
