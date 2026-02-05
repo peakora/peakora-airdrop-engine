e384776ea22656cbe85c3a454747eba6561c4cfc
peakora <peakora.network@gmail.com>
2026-02-05 22:39:48 +0300

    Update network.js

--- network/network.js
+++ network/network.js
@@ -1,16 +1,13 @@
-console.log("RPC_URLS from env:", process.env.RPC_URLS);
-
 const { ethers } = require("ethers");
+require("dotenv").config();
 
 function getProvider() {
   const urls = process.env.RPC_URLS ? process.env.RPC_URLS.split(",") : [];
   if (urls.length === 0) {
     throw new Error("No RPC_URLS defined in .env");
   }
-
-  // Try the first valid URL
   const url = urls[0].trim();
-  console.log(`Using RPC provider: ${url}`);
+  console.log("Using RPC provider:", url);
   return new ethers.JsonRpcProvider(url);
 }
 
